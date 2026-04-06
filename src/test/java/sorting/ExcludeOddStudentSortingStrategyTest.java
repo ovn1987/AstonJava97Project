8051feb421e36184a6a022a98e081381fa8d7af9
DmitriyPershukov <dmitriypershukov@gmail.com>
2026-04-06 20:02:09 +0500

    refactored sorting strategy tests

--- src/test/java/sorting/ExcludeOddStudentSortingStrategyTest.java
+++ src/test/java/sorting/ExcludeOddStudentSortingStrategyTest.java
@@ -7,45 +7,28 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExcludeOddStudentSortingStrategyTest {
-    @Test
-    void testSort(){
-        List<Student> listToSort = new ArrayList<>();
-        Student.Builder builder = new Student.Builder("John McMilson",
-                356,
-                4.3,
-                545697686);
-        listToSort.add(builder.build());
-        builder = new Student.Builder("Maya McMilson",
-                145,
-                4.7,
-                445697685);
-        listToSort.add(builder.build());
-        builder = new Student.Builder("Billy Green",
-                845,
-                3.9,
-                245697686);
-        listToSort.add(builder.build());
-        builder = new Student.Builder("Alice Hide",
-                256,
-                4.0,
-                645697685);
-        listToSort.add(builder.build());
-        builder = new Student.Builder("George Smith",
-                146,
-                3.5,
-                545697686);
-        listToSort.add(builder.build());
+public class ExcludeOddStudentSortingStrategyTest extends StudentSortingStrategyTest{
+    @Override
+    protected StudentSortingStrategy getStrategy() {
+        return new ExcludeOddStudentSortingStrategy();
+    }
 
-        List<Student> expectedList = List.of(listToSort.get(4),
+    @Override
+    protected List<Student> getExpectedList() {
+        List<Student> listToSort = getListToSort();
+        return List.of(listToSort.get(3),
                 listToSort.get(1),
                 listToSort.get(2),
-                listToSort.get(3),
+                listToSort.get(4),
                 listToSort.get(0));
+    }
 
-        StudentSorter.setStudentSortingStrategy(new ExcludeOddStudentSortingStrategy());
-        StudentSorter.sort(listToSort, StudentField.GROUP_NUMBER);
-
-        Assertions.assertIterableEquals(listToSort, expectedList);
+    @Test
+    void testSortingByNonNumericFieldThrowsException(){
+        StudentSorter.setStudentSortingStrategy(getStrategy());
+        List<Student> list = new ArrayList<>();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            StudentSorter.sort(list, StudentField.FULL_NAME);
+        });
     }
 }
