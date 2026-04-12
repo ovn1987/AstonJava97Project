823a8ae05dbdd34ea29f42444d6fd566c84b136d
DmitriyPershukov <dmitriypershukov@gmail.com>
2026-04-12 22:14:42 +0500

    added StudentsFrequency tests that check that methods properly work with empty collection and null collection

--- src/test/java/frequency/StudentFrequencyTest.java
+++ src/test/java/frequency/StudentFrequencyTest.java
@@ -5,6 +5,7 @@ import model.Student;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentFrequencyTest {
@@ -16,5 +17,22 @@ public class StudentFrequencyTest {
 
         List<Student> students = filler.fill(0);
         Assertions.assertEquals(StudentFrequency.getStudentFrequencyConcurrently(students, student), 4);
+    }
+
+    @Test
+    void testGetStudentFrequencyConcurrentlyEmptyCollection(){
+        Student student = new Student.Builder("Козлова Ольга Владимировна",
+                2, 3.6, 901234).build();
+        List<Student> students = new ArrayList<>();
+        Assertions.assertEquals(StudentFrequency.getStudentFrequencyConcurrently(students, student), 0);
+    }
+
+    @Test
+    void testNullCollectionThrowsException(){
+        Student student = new Student.Builder("Козлова Ольга Владимировна",
+                2, 3.6, 901234).build();
+        List<Student> students = null;
+        Assertions.assertThrows(NullPointerException.class, () ->
+                StudentFrequency.getStudentFrequencyConcurrently(students, student));
     }
 }
